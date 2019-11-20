6797ca164baaa3f6250b8d914819d3c983bfac8c
Tom Bennett <thomas.bennett@williamhill.com>
2019-11-20 10:42:34 +0000

    Added some input validation and a final profit/loss result when finishing.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
@@ -8,25 +9,62 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.println("How much money do you want to start with?");
+        int balance = 0;
+        boolean isValidBalance = false;
+        while (!isValidBalance) {
+        try {
+            balance = Integer.parseInt(scanner.nextLine());
+        } catch(Exception e) {
+            System.out.println("Invalid type. Please enter a number:");
+        }
+        if (balance <= 0) {
+            System.out.println("Please enter a positive value:");
+        } else {
+            isValidBalance = true;
+        }
+        }
+        int initialBalance = balance;
         boolean isReplaying = true;
-	    while (isReplaying) {
-            // Player choice:
-
-            String playerChoice = "null";
-            boolean isNotValidChoice = true;
-            String[] choices = {"Rock", "Paper", "Scissors", "Random"};
-            int choicesLength = choices.length;
-        while (isNotValidChoice) {
-            System.out.println("Please choose an option: ");
-            playerChoice = scanner.nextLine();
-            // Case insensitive
-            playerChoice = playerChoice.substring(0, 1).toUpperCase() + playerChoice.substring(1).toLowerCase();
-            for (String choice: choices) {
-                if (playerChoice.equals(choice)) {
-                    isNotValidChoice = false;
+        while (isReplaying) {
+            //Player bet:
+            int bet = 0;
+            boolean isValidBet = false;
+            while (!isValidBet) {
+                try {
+                    System.out.println("Please enter a bet: ");
+                    bet = Integer.parseInt(scanner.nextLine());
+                } catch(Exception e) {
+                    System.out.println("Invalid type. Please enter a number:");
+                }
+                if (bet > balance) {
+                    System.out.println("You don't have enough money for that bet. Please enter a lower amount:");
+                } else if (bet <= 0) {
+                    System.out.println("Please enter a positive amount: ");
+                } else {
+                    isValidBet = true;
                 }
             }
-        }
+
+            // Player choice:
+
+            String playerChoice = "Random";
+            boolean isValidChoice = false;
+            String[] choices = {"Rock", "Paper", "Scissors", "Random"};
+            int choicesLength = choices.length;
+            while (!isValidChoice) {
+                System.out.println("Please choose an option: ");
+                playerChoice = scanner.nextLine();
+                // Case insensitive
+                playerChoice = playerChoice.substring(0, 1).toUpperCase() + playerChoice.substring(1).toLowerCase();
+                for (String choice : choices) {
+                    if (playerChoice.equals(choice)) {
+                        isValidChoice = true;
+                        break;
+                    }
+                }
+            }
+
             if (playerChoice.equals("Random")) {
                 Random rPlayer = new Random();
                 int randomPlayerNumber = rPlayer.nextInt(choicesLength);
@@ -52,27 +90,42 @@ public class Main {
 
             if (result == 1) {
                 System.out.println("You won!");
+                balance += bet;
             } else if (result == 0) {
                 System.out.println("You drew!");
             } else {
                 System.out.println("You lost!");
+                balance -= bet;
             }
 
             // Replay:
-            System.out.println("Play again? (y/n)");
-            String replay = "null";
-            while (true) {
-                replay = scanner.nextLine();
-                if (replay.equals("y")) {
-                    isReplaying = true;
-                    break;
-                } else if ( replay.equals("n")) {
-                    isReplaying = false;
-                    break;
-                } else {
-                    System.out.println("Invalid input. Please enter either y or n.");
+            System.out.println("Your balance is: " + balance);
+            if (balance == 0) {
+                System.out.println("You have no more money left to bet! Credit betting is not permitted here.");
+                isReplaying = false;
+            } else {
+                System.out.println("Play again? (y/n)");
+                String replay;
+                while (true) {
+                    replay = scanner.nextLine();
+                    if (replay.equals("y")) {
+                        break;
+                    } else if (replay.equals("n")) {
+                        isReplaying = false;
+                        break;
+                    } else {
+                        System.out.println("Invalid input. Please enter either y or n.");
+                    }
                 }
             }
+        }
+        int balanceDifference = balance - initialBalance;
+        if (balanceDifference > 0) {
+            System.out.println("You made a profit of £" + balanceDifference + "!");
+        } else if (balanceDifference == 0) {
+            System.out.println("You broke even!");
+        } else {
+            System.out.println("You lost £" + Math.abs(balanceDifference) + ".");
         }
     }
 }
